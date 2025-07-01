912b4ccca56fb4cb28e0777001089a4d4d067668
Helio Sales Jr <heliosalesjr@gmail.com>
2025-06-30 22:28:48 -0400

    added the footer to the layout

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
+export default function Footer() {
+    return (
+      <footer className="w-full py-6 px-6 mt-12 bg-gray-100 text-center text-sm text-gray-600">
+        <p>
+          © {new Date().getFullYear()} Sunset Locator. All rights reserved. Created by Hélio Sales Jr.
+        </p>
+      </footer>
+    );
+  }
+  
